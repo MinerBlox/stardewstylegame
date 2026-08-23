477d834188dfe710d0c7655160b84d58f1513589
MinerBlox <52011045+MinerBlox@users.noreply.github.com>
2026-08-23 22:21:49 +0100

    Add game configuration

---
+++ js/config.js
@@ -0,0 +1,5 @@
+export const CONFIG={
+  BASE_W:960,BASE_H:540,TILE:32,WORLD_W:2400,WORLD_H:1760,
+  PLAYER_SPEED:150,TIME_SCALE:7.5,AUTOSAVE_MS:15000,SAVE_KEY:'brackenvale-save-v1',SETTINGS_KEY:'brackenvale-settings-v1'
+};
+export const PALETTE={grass:'#5e7e45',grass2:'#587640',path:'#b29662',path2:'#9b7c50',water:'#426f84',water2:'#4f8294',wood:'#765237',roof:'#7c463d',stone:'#77736a',stone2:'#5c5a54',tree:'#355638',tree2:'#4d7046',trunk:'#5e422c',ink:'#201712'};
