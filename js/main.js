66093060c80dd10084064d25c1559deb2aa8d89d
MinerBlox <52011045+MinerBlox@users.noreply.github.com>
2026-08-24 10:28:22 +0100

    Load generated art depth cleanup

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import './core/polish.js';
 import './core/world-polish.js';
 import './core/generated-art.js';
+import './core/generated-depth.js';
 import {Game} from './core/Game.js';
 const canvas=document.getElementById('game');
 new Game(canvas);
