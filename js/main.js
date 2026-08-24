1d37876e579a484603dd53fd8dfc7a683127da6f
MinerBlox <52011045+MinerBlox@users.noreply.github.com>
2026-08-24 10:27:32 +0100

    Load generated sprite atlas renderer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import './core/polish.js';
 import './core/world-polish.js';
+import './core/generated-art.js';
 import {Game} from './core/Game.js';
 const canvas=document.getElementById('game');
 new Game(canvas);
